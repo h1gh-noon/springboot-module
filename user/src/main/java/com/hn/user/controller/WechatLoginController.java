a6cf49dba9282b2e57a5428dddf730292025386a
HighNoon_laptop <h1gh-noon@outlook.com>
2023-11-30 15:56:22 +0800

    微信扫码授权

--- user/src/main/java/com/hn/user/controller/WechatLoginController.java
+++ user/src/main/java/com/hn/user/controller/WechatLoginController.java
@@ -38,13 +38,19 @@ public class WechatLoginController {
     @Value("${Wechat.mpAppSecret}")
     private String mpAppSecret;
 
+    @Value("${Wechat.openAppId}")
+    private String openAppId;
+
+    @Value("${Wechat.openAppSecret}")
+    private String openAppSecret;
+
     @Value("${Wechat.domainName}")
     private String domainName;
 
-    @Operation(summary = "微信授权引导url", description = "微信授权引导url")
-    @RequestMapping("/loginByWechatUrl")
-    public String loginByWechatUrl() throws UnsupportedEncodingException {
-        String redirect_uri = "http://"+ domainName +"/api/user/authByWechat/loginByWechat";
+    @Operation(summary = "微信客户端授权引导url", description = "微信客户端授权引导url")
+    @RequestMapping("/loginByWechatClientUrl")
+    public String loginByWechatClientUrl() throws UnsupportedEncodingException {
+        String redirect_uri = "http://" + domainName + "/api/user/authByWechat/loginByWechatClient";
         String encoderUri = URLEncoder.encode(redirect_uri, "GBK");
 
         String url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri" +
@@ -53,18 +59,45 @@ public class WechatLoginController {
         return "redirect:" + String.format(url, mpAppId, encoderUri, "code", "snsapi_userinfo");
     }
 
-    @Operation(summary = "微信授权url", description = "微信授url")
-    @RequestMapping("/loginByWechat")
-    public Object loginByWechat(@RequestParam String code,
-                                @RequestParam(required = false) String state) {
-
-        log.info("code={}, state={}", code, state);
+    @Operation(summary = "微信客户端授权url", description = "微信客户端授url")
+    @RequestMapping("/loginByWechatClient")
+    public Object loginByWechatClient(@RequestParam String code) {
 
         String urlBuild = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type" +
                 "=authorization_code";
         String url = String.format(urlBuild, mpAppId, mpAppSecret, code);
         String res = restTemplate.getForObject(url, String.class);
-        JSONObject resObj = JSON.parseObject(res);
+        log.info("微信客户端授权 Wechat接口返回={}", res);
+        return loginByWechat(res);
+    }
+
+    @Operation(summary = "微信扫码授权引导url", description = "微信扫码授权引导url")
+    @RequestMapping("/loginByWechatQRCodeUrl")
+    public String loginByWechatQRCodeUrl() throws UnsupportedEncodingException {
+        String redirect_uri = "http://" + domainName + "/api/user/authByWechat/loginByWechatQRCode";
+        String encoderUri = URLEncoder.encode(redirect_uri, "GBK");
+
+        String url = "https://open.weixin.qq.com/connect/qrconnect?appid=%s&redirect_uri=%s&response_type" +
+                "=code&scope=snsapi_login#wechat_redirect";
+
+        return "redirect:" + String.format(url, openAppId, encoderUri);
+    }
+
+    @Operation(summary = "微信扫码授权url", description = "微信扫码授url")
+    @RequestMapping("/loginByWechatQRCode")
+    public Object loginByWechatQRCode(@RequestParam String code) {
+
+        String urlBuild = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type" +
+                "=authorization_code";
+        String url = String.format(urlBuild, openAppId, openAppSecret, code);
+
+        String res = restTemplate.getForObject(url, String.class);
+        log.info("微信扫码授权 Wechat接口返回={}", res);
+        return loginByWechat(res);
+    }
+
+    private Object loginByWechat(String result) {
+        JSONObject resObj = JSON.parseObject(result);
         if (resObj != null && !resObj.containsKey("errcode")) {
             // success
             log.info("获取微信返回信息={}", resObj);
@@ -74,12 +107,12 @@ public class WechatLoginController {
 
             WechatUserInfoDto userInfoDto = JSON.parseObject(
                     restTemplate.getForObject(String.format(getUserInfoBuild,
-                    accToken, openid), String.class), WechatUserInfoDto.class);
+                            accToken, openid), String.class), WechatUserInfoDto.class);
 
             if (userInfoDto != null && userInfoDto.getErrcode() == null) {
                 // success
                 String token = wechatUserService.wechatUserLogin(userInfoDto);
-                return "redirect:http://192.168.110.179:5173/login?token=" + token;
+                return "redirect:http://localhost:5173/login?token=" + token;
             }
 
         }
